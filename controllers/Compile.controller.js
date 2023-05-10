73565b7682a3a18505d79631f83c0615afff3122
mohamed-dhia-ben-amar <mohameddhia.benamar@esprit.tn>
2023-05-10 20:44:43 +0100

    5/10/23

--- controllers/Compile.controller.js
+++ controllers/Compile.controller.js
@@ -1,8 +1,5 @@
 import fetch from 'node-fetch'
 
-
-
-
 export async function CompileCode(req, res, next) {
 
     var code = req.body.code
@@ -15,18 +12,27 @@ export async function CompileCode(req, res, next) {
         clientSecret: "cde26308ed67991743c4fefedc06867df45eae3a5aa7fe41d6edad57b4acd824"
     };
 
+    const body = `{
+        "script": "${program.script.replace(/"/g, '\\"').replace(/\n/g, '\\n').replace(/\t/g, '\\t')}",
+        "language": "${program.language}",
+        "versionIndex": "${program.versionIndex}",
+        "clientId": "${program.clientId}",
+        "clientSecret": "${program.clientSecret}"
+    }`;
+
     fetch('https://api.jdoodle.com/v1/execute', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(program)
+        body: body
     })
         .then(response => response.json())
         .then(data => {
             res.json({
                 data
             })
+            console.log(program.script.replace(/"/g, '\\"').replace(/\n/g, '\\n').replace("   ", '\\t'))
         })
         .catch(error => {
             res.json({
